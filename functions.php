1f35e37de689b1fdd525722b69c7103e84b32b11
HariboPenguin <HariboPenguin@me.com>
2013-01-31 19:23:21 +0000

    Added styling changes for wp-login page

--- functions.php
+++ functions.php
@@ -11,5 +11,14 @@ function register_menus() {
 	);
 }
 
+function login_stylesheet() {
+	wp_enqueue_style( 'login_stylesheet', get_template_directory_uri().'/css/login.css');
+}
+add_action('login_head', 'login_stylesheet');
+
+function login_logo_url($url) {
+	return 'http://vaulthost.d-tomlinson.co.uk';
+}
+add_filter('login_headerurl','login_logo_url');
 
 ?>
