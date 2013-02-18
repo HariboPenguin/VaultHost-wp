db26dde2c5fb27fe757a58b9c2159ac48e59e681
HariboPenguin <HariboPenguin@me.com>
2013-02-18 19:39:49 +0000

    Changed package thingy

--- 404.php
+++ 404.php
@@ -3,8 +3,8 @@
  * The template for displaying 404 pages (Not Found).
  *
  * @package WordPress
- * @subpackage Bare 
- * @since Bare 1.0
+ * @subpackage VaultHost 
+ * @since VaultHost 1.0
  */
 
 get_header(); ?>
@@ -13,9 +13,9 @@ get_header(); ?>
 		<div id="content" role="main">
 
 			<div id="post-0" class="post error404 not-found">
-				<h1 class="entry-title"><?php _e( 'Not Found', 'bare' ); ?></h1>
+				<h1 class="entry-title"><?php _e( 'Not Found', 'vaulthost' ); ?></h1>
 				<div class="entry-content">
-					<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'bare' ); ?></p>
+					<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'vaulthost' ); ?></p>
 					<?php get_search_form(); ?>
 				</div><!-- .entry-content -->
 			</div><!-- #post-0 -->
