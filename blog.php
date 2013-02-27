e224f5bdcc8c7dae0696fc257e4ff62c0187ae5e
HariboPenguin <HariboPenguin@me.com>
2013-02-27 19:31:00 +0000

    Renamed home page template to stop clash with blog page

--- blog.php
+++ blog.php
@@ -1,15 +1,16 @@
 <?php 
 /*
-Template Name: Blog
+* Template Name: Blog
 */
 ?>
 
 <?php get_header(); ?>
 
 <div class="wrapper">
-<?php if (have_posts()) : while (have_posts()) : the_post();?>
-<?php the_content(); ?>
-<?php endwhile; endif; ?>
-
+	<h1>Blog</h1>
+	<?php if (have_posts()) : while (have_posts()) : the_post();?>
+	<?php the_content(); ?>
+	<?php endwhile; endif; ?>
 </div>
+
 <?php get_footer(); ?>
