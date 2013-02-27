866baa11c94490e91d0ec46491140b3f628045db
HariboPenguin <HariboPenguin@me.com>
2013-02-27 18:52:48 +0000

    Edited wrong file :P

--- home.php
+++ home.php
@@ -2,12 +2,8 @@
 
 <?php get_header(); ?>
 
-<div class="wrapper">
-
 <?php if (have_posts()) : while (have_posts()) : the_post();?>
 <?php the_content(); ?>
 <?php endwhile; endif; ?>
-
-</div>
 
 <?php get_footer(); ?>
