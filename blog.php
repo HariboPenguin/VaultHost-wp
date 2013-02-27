7d79344781c4e8e0bd6a06b80139f3fd3ae1556e
HariboPenguin <HariboPenguin@me.com>
2013-02-27 18:53:22 +0000

    Added wrapper class to blog page

--- blog.php
+++ blog.php
@@ -6,8 +6,10 @@ Template Name: Blog
 
 <?php get_header(); ?>
 
+<div class="wrapper">
 <?php if (have_posts()) : while (have_posts()) : the_post();?>
 <?php the_content(); ?>
 <?php endwhile; endif; ?>
 
+</div>
 <?php get_footer(); ?>
