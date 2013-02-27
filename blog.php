3ce41c1f5ca728840db6420a3cf2d1a35b5a7dc1
HariboPenguin <HariboPenguin@me.com>
2013-02-27 20:06:35 +0000

    Started work on blog page

--- blog.php
+++ blog.php
@@ -8,9 +8,15 @@
 
 <div class="wrapper">
 	<h1>Blog</h1>
-	<?php if (have_posts()) : while (have_posts()) : the_post();?>
-	<?php the_content(); ?>
-	<?php endwhile; endif; ?>
+	<?php $the_query = new WP_Query('showposts=3'); ?>
+	<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+	<article>
+		<h3><?php echo get_the_date(); ?></h3>
+		<h3><?php the_time(); ?></h3>
+		<h2><a href="<?php the_permalink(); ?>" title=""><?php the_title(); ?></a></h2>
+		<p><?php the_excerpt(__('(more...)')); ?></p>
+	</article>
+	<?php endwhile; ?>
 </div>
 
 <?php get_footer(); ?>
