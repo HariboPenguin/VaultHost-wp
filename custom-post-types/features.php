280db26b4205cc2f5f816f2d584b020825acf7d8
HariboPenguin <HariboPenguin@me.com>
2013-03-03 17:40:15 +0000

    Added custom post update messages and added contextural help

--- custom-post-types/features.php
+++ custom-post-types/features.php
@@ -1,5 +1,9 @@
 <?php 
 
+/* ------------------------------------------------------------------------ * 
+ * Add Features Custom Post Type
+ * ------------------------------------------------------------------------ */   
+
 add_action('init', 'init_features_post_type');
 
 function init_features_post_type() {
@@ -32,6 +36,10 @@ function init_features_post_type() {
 	);
 }
 
+/* ------------------------------------------------------------------------ * 
+ * Add custom columns to table
+ * ------------------------------------------------------------------------ */   
+
 add_filter('manage_edit-features_columns', 'feature_edit_columns');
 
 function feature_edit_columns() {
@@ -58,5 +66,40 @@ function features_custom_columns($column) {
 			break;
 	}
 }
+
+/* ------------------------------------------------------------------------ * 
+ * Add custom messages
+ * ------------------------------------------------------------------------ */
+
+function features_updated_messages($messages) {
+	global $post, $post_ID;
+	$messages['features'] = array(
+		1 => sprintf( __('Feature Updated. <a href="%s">View Feature</a>'), esc_url( get_home_url() )),
+		4 => __('Feature updated.'),
+		6 => sprintf( __('Feature Published. <a href="%s">View Feature</a>'), esc_url( get_home_url() )),
+		7 => __('Feature Saved.'),
+		8 => sprintf( __('Feature Submitted. <a href="%s">View Feature</a>'), esc_url( get_home_url() )),
+	);
+	return $messages;
+}
+
+add_filter('post_updated_messages', 'features_updated_messages');
+
+function my_contextual_help( $contextual_help, $screen_id, $screen ) { 
+	if ( 'edit-features' == $screen->id ) {
+
+		$contextual_help = '<h2>Features</h2>
+		<p>Features show the various services and support that the company offers on the home page. You can see a list of them on this page in reverse chronological order - the latest one we added is first.</p> 
+		<p>You can view/edit the details of each feature by clicking on its name, or you can perform bulk actions using the dropdown menu and selecting multiple items.</p>';
+
+	} elseif ( 'features' == $screen->id ) {
+
+		$contextual_help = '<h2>Editing features</h2>
+		<p>This page allows you to view/modify features that are displayed on the home page. Please make sure to fill out the available boxes with the appropriate details (feature image, title, description).</p>';
+
+	}
+	return $contextual_help;
+}
+add_action( 'contextual_help', 'my_contextual_help', 10, 3 );
 
 ?>
