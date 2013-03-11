716c1fc5390500bb74064e6641de0cabb9f60c1b
HariboPenguin <HariboPenguin@me.com>
2013-03-11 17:23:00 +0000

    Started work on dynamic lowest price for service

--- page-home.php
+++ page-home.php
@@ -16,19 +16,26 @@
 			<p>Get up and running within minutes</p>
 		</div>
 		<div id="serviceboxcontainer">
+			<?php $webhosting_packages = new WP_Query(array('post_type' => 'webhosting', 'orderby' => 'package_price', 'order' => 'DESC')); ?>
+			<?php if ($webhosting_packages->have_posts()) { ?>
 			<div class="servicebox">
 				<img class="alignnone size-full wp-image-100" alt="Web Hosting" src="http://vaulthost.d-tomlinson.co.uk/wp-content/uploads/2013/01/web-hosting-icon.png" />
 				<h4 class="serviceboxtitle">Web Hosting</h4>
 				<p class="serviceboxdescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum magna neque, accumsan sit amet tempus.</p>
 				<p class="serviceboxprice">From
 					<br>
-					<span>£2.99</span> p/m
+					<?php while ($webhosting_packages->have_posts()) {
+						$webhosting_packages->the_post();
+					?>
+					<span><?php echo '£' . floatval(get_post_meta( $post->ID, 'package_price', true)); ?></span> p/m
+					<?php } ?>
 				</p>
 				<a class="serviceboxmorebutton" href="#">Learn More</a>
 				<div style="clear: both;"></div>
 			</div>
+			<?php } ?>
 			<div class="servicebox">
-				<img class="alignnone size-full wp-image-100" alt="Web Hosting" src="http://vaulthost.d-tomlinson.co.uk/wp-content/uploads/2013/01/VPS-icon.png" />
+				<img class="alignnone size-full wp-image-100" alt="VPS" src="http://vaulthost.d-tomlinson.co.uk/wp-content/uploads/2013/01/VPS-icon.png" />
 				<h4 class="serviceboxtitle">VPS</h4>
 				<p class="serviceboxdescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum magna neque, accumsan sit amet tempus.</p>
 				<p class="serviceboxprice">From
@@ -39,7 +46,7 @@
 				<div style="clear: both;"></div>
 			</div>
 			<div class="servicebox">
-				<img class="alignnone size-full wp-image-100" alt="Web Hosting" src="http://vaulthost.d-tomlinson.co.uk/wp-content/uploads/2013/01/Dedicated-Servers-Icon.png" />
+				<img class="alignnone size-full wp-image-100" alt="Dedicated Servers" src="http://vaulthost.d-tomlinson.co.uk/wp-content/uploads/2013/01/Dedicated-Servers-Icon.png" />
 				<h4 class="serviceboxtitle">Dedicated Servers</h4>
 				<p class="serviceboxdescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum magna neque, accumsan sit amet tempus.</p>
 				<p class="serviceboxprice">From
