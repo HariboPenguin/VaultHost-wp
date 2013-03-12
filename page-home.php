f0ad5338a10ad3618c91c52e9002a0c506ece325
HariboPenguin <HariboPenguin@me.com>
2013-03-12 16:15:30 +0000

    Added dedicated server section to home page properly

--- page-home.php
+++ page-home.php
@@ -58,17 +58,27 @@
 				<div style="clear: both;"></div>
 			</div>
 			<?php } ?>
+			<?php $dedicatedservers_packages = new WP_Query(array('post_type' => 'dedicatedservers', 'meta_key' => 'package_price', 'orderby' => 'meta_value_num', 'order' => 'ASC')); ?>
+			<?php if ($dedicatedservers_packages->have_posts()) { ?>
 			<div class="servicebox">
 				<img class="alignnone size-full wp-image-100" alt="Dedicated Servers" src="http://vaulthost.d-tomlinson.co.uk/wp-content/uploads/2013/01/Dedicated-Servers-Icon.png" />
 				<h4 class="serviceboxtitle">Dedicated Servers</h4>
 				<p class="serviceboxdescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum magna neque, accumsan sit amet tempus.</p>
 				<p class="serviceboxprice">From
 					<br>
-					<span>£2.99</span> p/m
+					<?php $counter = 0; ?>
+					<?php while ($dedicatedservers_packages->have_posts() && $counter < 1) {
+						$dedicatedservers_packages->the_post();
+						$dedicatedservers_price = floatval(get_post_meta( $post->ID, 'package_price', true));
+					?>
+					<span><?php echo '£' . number_format($dedicatedservers_price, 2); ?></span> p/m
+					<?php $counter++; ?>
+					<?php } ?>
 				</p>
 				<a class="serviceboxmorebutton" href="#">Learn More</a>
 				<div style="clear: both;"></div>
 			</div>
+			<?php } ?>
 		</div>
 	</div>
 </div>
