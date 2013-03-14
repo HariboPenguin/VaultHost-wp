b062e98d3adc476cb2441e0e9d15f358c4c36a57
HariboPenguin <HariboPenguin@me.com>
2013-03-14 15:54:43 +0000

    Added gray styling to pricing table

--- page-vps.php
+++ page-vps.php
@@ -14,13 +14,13 @@
 		<?php $vps_packages = new WP_Query(array('post_type' => 'vps', 'meta_key' => 'package_price', 'orderby' => 'meta_value_num', 'order' => 'ASC')); ?>
 		<?php if ($vps_packages->have_posts()) { ?>
 			<div class="option-descriptions">
-				<div class="pricing-option-desc ram-option-desc"><p>RAM:</p></div>
+				<div class="pricing-option-desc ram-option-desc gray"><p>RAM:</p></div>
 				<div class="pricing-option-desc cpucores-option-desc"><p>CPU Cores:</p></div>
-				<div class="pricing-option-desc storage-option-desc"><p>Storage Space:</p></div>
+				<div class="pricing-option-desc storage-option-desc gray"><p>Storage Space:</p></div>
 				<div class="pricing-option-desc bandwidth-option-desc"><p>Bandwidth:</p></div>
-				<div class="pricing-option-desc ipv4-option-desc"><p>IPv4 Addresses:</p></div>
+				<div class="pricing-option-desc ipv4-option-desc gray"><p>IPv4 Addresses:</p></div>
 				<div class="pricing-option-desc ipv6-option-desc"><p>IPv6 Addresses:</p></div>
-				<div class="pricing-option-desc network-option-desc"><p>Network Speed:</p></div>
+				<div class="pricing-option-desc network-option-desc gray"><p>Network Speed:</p></div>
 			</div>
 			<?php while ($vps_packages->have_posts()) {
 				$vps_packages->the_post(); ?>
@@ -30,13 +30,13 @@
 						<p class="package-price"><?php echo '£' . number_format(floatval(get_post_meta( $post->ID, 'package_price', true)),2); ?></p>
 					</div>
 					<div class="package-details">
-						<div class="pricing-option ram-option"><?php echo intval(get_post_meta( $post->ID, 'package_ram', true)) . 'MB'; ?></div>
+						<div class="pricing-option ram-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_ram', true)) . 'MB'; ?></div>
 						<div class="pricing-option cpucores-option"><?php echo intval(get_post_meta( $post->ID, 'package_cpucores', true)); ?></div>
-						<div class="pricing-option storage-option"><?php echo intval(get_post_meta( $post->ID, 'package_storage', true)) . 'GB'; ?></div>
+						<div class="pricing-option storage-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_storage', true)) . 'GB'; ?></div>
 						<div class="pricing-option bandwidth-option"><?php echo intval(get_post_meta( $post->ID, 'package_bandwidth', true)) . 'GB'; ?></div>
-						<div class="pricing-option ipv4-option"><?php echo intval(get_post_meta( $post->ID, 'package_ipv4', true)); ?></div>
+						<div class="pricing-option ipv4-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_ipv4', true)); ?></div>
 						<div class="pricing-option ipv6-option"><?php echo intval(get_post_meta( $post->ID, 'package_ipv6', true)); ?></div>
-						<div class="pricing-option network-option"><?php echo intval(get_post_meta( $post->ID, 'package_network', true)) . 'Mbps'; ?></div>
+						<div class="pricing-option network-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_network', true)) . 'Mbps'; ?></div>
 					</div>
 				</div>
 			<?php } ?>
