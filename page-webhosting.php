906e9dea60b0041db747f2493749cb2cfbd7cc2e
HariboPenguin <HariboPenguin@me.com>
2013-03-14 15:26:08 +0000

    More styling changes to pricing table

--- page-webhosting.php
+++ page-webhosting.php
@@ -15,13 +15,13 @@
 		<?php $webhosting_packages = new WP_Query(array('post_type' => 'webhosting', 'meta_key' => 'package_price', 'orderby' => 'meta_value_num', 'order' => 'ASC')); ?>
 		<?php if ($webhosting_packages->have_posts()) { ?>
 			<div class="option-descriptions">
-				<div class="pricing-option-desc storage-option-desc"><p>Storage Space:</p></div>
+				<div class="pricing-option-desc storage-option-desc gray"><p>Storage Space:</p></div>
 				<div class="pricing-option-desc bandwidth-option-desc"><p>Bandwidth:</p></div>
-				<div class="pricing-option-desc domains-option-desc"><p>Domains:</p></div>
+				<div class="pricing-option-desc domains-option-desc gray"><p>Domains:</p></div>
 				<div class="pricing-option-desc subdomains-option-desc"><p>Subdomains:</p></div>
-				<div class="pricing-option-desc emailaccounts-option-desc"><p>Email Accounts:</p></div>
+				<div class="pricing-option-desc emailaccounts-option-desc gray"><p>Email Accounts:</p></div>
 				<div class="pricing-option-desc dbs-option-desc"><p>MySQL Databases:</p></div>
-				<div class="pricing-option-desc ftpaccounts-option-desc"><p>FTP Accounts:</p></div>
+				<div class="pricing-option-desc ftpaccounts-option-desc gray"><p>FTP Accounts:</p></div>
 			</div>
 			<?php while ($webhosting_packages->have_posts()) {
 				$webhosting_packages->the_post(); ?>
@@ -31,13 +31,13 @@
 						<p class="package-price"><?php echo '£' . number_format(floatval(get_post_meta( $post->ID, 'package_price', true)),2); ?></p>
 					</div>
 					<div class="package-details">
-						<div class="pricing-option storage-option"><?php echo intval(get_post_meta( $post->ID, 'package_storage', true)) . 'GB'; ?></div>
+						<div class="pricing-option storage-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_storage', true)) . 'GB'; ?></div>
 						<div class="pricing-option bandwidth-option"><?php echo intval(get_post_meta( $post->ID, 'package_bandwidth', true)) . 'GB'; ?></div>
-						<div class="pricing-option domains-option"><?php echo intval(get_post_meta( $post->ID, 'package_domains', true)); ?></div>
+						<div class="pricing-option domains-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_domains', true)); ?></div>
 						<div class="pricing-option subdomains-option"><?php echo intval(get_post_meta( $post->ID, 'package_subdomains', true)); ?></div>
-						<div class="pricing-option emailaccounts-option"><?php echo intval(get_post_meta( $post->ID, 'package_emailaccounts', true)); ?></div>
+						<div class="pricing-option emailaccounts-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_emailaccounts', true)); ?></div>
 						<div class="pricing-option dbs-option"><?php echo intval(get_post_meta( $post->ID, 'package_dbs', true)); ?></div>
-						<div class="pricing-option ftpaccounts-option"><?php echo intval(get_post_meta( $post->ID, 'package_ftpaccounts', true)); ?></div>
+						<div class="pricing-option ftpaccounts-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_ftpaccounts', true)); ?></div>
 					</div>
 				</div>
 			<?php } ?>
