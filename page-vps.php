f43b5b449338f61b8c4e0699a618e32e02f7d2d9
HariboPenguin <HariboPenguin@me.com>
2013-03-14 12:12:56 +0000

    Added page templates for hosting services

---
+++ page-vps.php
@@ -0,0 +1,11 @@
+<?php /* Template Name: VPS */ ?>
+
+<?php get_header(); ?>
+
+<div class="wrapper">
+	<?php if (have_posts()) : while (have_posts()) : the_post();?>
+	<?php the_content(); ?>
+	<?php endwhile; endif; ?>
+</div>
+
+<?php get_footer(); ?>
