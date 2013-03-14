36feaf06a501366806648589154b9e1fb4cc0708
HariboPenguin <HariboPenguin@me.com>
2013-03-14 13:07:35 +0000

    Moved option descriptions out of while loop

--- page-webhosting.php
+++ page-webhosting.php
@@ -14,18 +14,17 @@
 	<div class="wrapper">
 		<?php $webhosting_packages = new WP_Query(array('post_type' => 'webhosting', 'meta_key' => 'package_price', 'orderby' => 'meta_value_num', 'order' => 'ASC')); ?>
 		<?php if ($webhosting_packages->have_posts()) { ?>
-
+			<div class="option-descriptions">
+				<div class="storage-option-desc"><p>Storage Space:</p></div>
+				<div class="bandwidth-option-desc"><p>Bandwidth:</p></div>
+				<div class="domains-option-desc"><p>Domains:</p></div>
+				<div class="subdomains-option-desc"><p>Subdomains:</p></div>
+				<div class="emailaccounts-option-desc"><p>Email Accounts:</p></div>
+				<div class="dbs-option-desc"><p>MySQL Databases:</p></div>
+				<div class="ftpaccounts-option-desc"><p>FTP Accounts:</p></div>
+			</div>
 			<?php while ($webhosting_packages->have_posts()) {
 				$webhosting_packages->the_post(); ?>
-				<div class="option-descriptions">
-					<div class="storage-option-desc"><p>Storage Space:</p></div>
-					<div class="bandwidth-option-desc"><p>Bandwidth:</p></div>
-					<div class="domains-option-desc"><p>Domains:</p></div>
-					<div class="subdomains-option-desc"><p>Subdomains:</p></div>
-					<div class="emailaccounts-option-desc"><p>Email Accounts:</p></div>
-					<div class="dbs-option-desc"><p>MySQL Databases:</p></div>
-					<div class="ftpaccounts-option-desc"><p>FTP Accounts:</p></div>
-				</div>
 				<div class="package">
 					<div class="package-header">
 						<h4 class="package-title"><?php echo the_title(); ?></h4>
