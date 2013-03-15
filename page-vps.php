df3e40712a03edb424ddecc4a6b8b57397b0d69d
HariboPenguin <HariboPenguin@me.com>
2013-03-15 16:04:54 +0000

    Start pricing tables from scratch

--- page-vps.php
+++ page-vps.php
@@ -8,41 +8,6 @@
 	<?php endwhile; endif; ?>
 </div>
 
-<!-- Package Pricing Table -->
-<div class="packages">
-	<div class="wrapper">
-		<?php $vps_packages = new WP_Query(array('post_type' => 'vps', 'meta_key' => 'package_price', 'orderby' => 'meta_value_num', 'order' => 'ASC')); ?>
-		<?php if ($vps_packages->have_posts()) { ?>
-			<div class="option-descriptions">
-				<div class="pricing-option-desc ram-option-desc gray"><p>RAM:</p></div>
-				<div class="pricing-option-desc cpucores-option-desc"><p>CPU Cores:</p></div>
-				<div class="pricing-option-desc storage-option-desc gray"><p>Storage Space:</p></div>
-				<div class="pricing-option-desc bandwidth-option-desc"><p>Bandwidth:</p></div>
-				<div class="pricing-option-desc ipv4-option-desc gray"><p>IPv4 Addresses:</p></div>
-				<div class="pricing-option-desc ipv6-option-desc"><p>IPv6 Addresses:</p></div>
-				<div class="pricing-option-desc network-option-desc gray"><p>Network Speed:</p></div>
-			</div>
-			<?php while ($vps_packages->have_posts()) {
-				$vps_packages->the_post(); ?>
-				<div class="package">
-					<div class="package-header">
-						<h4 class="package-title"><?php echo the_title(); ?></h4>
-						<p class="package-price"><?php echo '£' . number_format(floatval(get_post_meta( $post->ID, 'package_price', true)),2); ?></p>
-					</div>
-					<div class="package-details">
-						<div class="pricing-option ram-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_ram', true)) . 'MB'; ?></div>
-						<div class="pricing-option cpucores-option"><?php echo intval(get_post_meta( $post->ID, 'package_cpucores', true)); ?></div>
-						<div class="pricing-option storage-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_storage', true)) . 'GB'; ?></div>
-						<div class="pricing-option bandwidth-option"><?php echo intval(get_post_meta( $post->ID, 'package_bandwidth', true)) . 'GB'; ?></div>
-						<div class="pricing-option ipv4-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_ipv4', true)); ?></div>
-						<div class="pricing-option ipv6-option"><?php echo intval(get_post_meta( $post->ID, 'package_ipv6', true)); ?></div>
-						<div class="pricing-option network-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_network', true)) . 'Mbps'; ?></div>
-						<div class="pricing-option order-option">Order Now</div>
-					</div>
-				</div>
-			<?php } ?>
-		<?php } ?>
-	</div>
-</div>
+
 
 <?php get_footer(); ?>
--- page-webhosting.php
+++ page-webhosting.php
@@ -9,42 +9,5 @@
 </div>
 
 
-<!-- Package Pricing Table -->
-<div class="packages">
-	<div class="wrapper">
-		<?php $webhosting_packages = new WP_Query(array('post_type' => 'webhosting', 'meta_key' => 'package_price', 'orderby' => 'meta_value_num', 'order' => 'ASC')); ?>
-		<?php if ($webhosting_packages->have_posts()) { ?>
-			<div class="option-descriptions">
-				<div class="pricing-option-desc storage-option-desc gray"><p>Storage Space:</p></div>
-				<div class="pricing-option-desc bandwidth-option-desc"><p>Bandwidth:</p></div>
-				<div class="pricing-option-desc domains-option-desc gray"><p>Domains:</p></div>
-				<div class="pricing-option-desc subdomains-option-desc"><p>Subdomains:</p></div>
-				<div class="pricing-option-desc emailaccounts-option-desc gray"><p>Email Accounts:</p></div>
-				<div class="pricing-option-desc dbs-option-desc"><p>MySQL Databases:</p></div>
-				<div class="pricing-option-desc ftpaccounts-option-desc gray"><p>FTP Accounts:</p></div>
-			</div>
-			<?php while ($webhosting_packages->have_posts()) {
-				$webhosting_packages->the_post(); ?>
-				<div class="package">
-					<div class="package-header">
-						<h4 class="package-title"><?php echo the_title(); ?></h4>
-						<p class="package-price"><?php echo '£' . number_format(floatval(get_post_meta( $post->ID, 'package_price', true)),2); ?></p>
-					</div>
-					<div class="package-details">
-						<div class="pricing-option storage-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_storage', true)) . 'GB'; ?></div>
-						<div class="pricing-option bandwidth-option"><?php echo intval(get_post_meta( $post->ID, 'package_bandwidth', true)) . 'GB'; ?></div>
-						<div class="pricing-option domains-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_domains', true)); ?></div>
-						<div class="pricing-option subdomains-option"><?php echo intval(get_post_meta( $post->ID, 'package_subdomains', true)); ?></div>
-						<div class="pricing-option emailaccounts-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_emailaccounts', true)); ?></div>
-						<div class="pricing-option dbs-option"><?php echo intval(get_post_meta( $post->ID, 'package_dbs', true)); ?></div>
-						<div class="pricing-option ftpaccounts-option gray"><?php echo intval(get_post_meta( $post->ID, 'package_ftpaccounts', true)); ?></div>
-						<div class="pricing-option order-option">Order Now</div>
-					</div>
-				</div>
-			<?php } ?>
-		<?php } ?>
-	</div>
-</div>
-
 
 <?php get_footer(); ?>
